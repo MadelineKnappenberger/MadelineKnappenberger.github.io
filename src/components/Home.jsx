1ea98c1ae170956d524418d2dc30598567e478a1
Maddie <41520265+MadelineKnappenberger@users.noreply.github.com>
2025-10-19 13:10:43 -0700

    change wording

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -255,7 +255,7 @@ export default function Home() {
         <section className="home-section about-me">
           <div className="section-inner">
             <h1>Hi! I'm Maddie!</h1>
-            <p>Data analyst & web developer. I build data-driven, accessible web experiences.</p>
+            <p>Web developer & data analyst. I build data-driven, accessible web experiences.</p>
             <a className="home-link" href="/contact">About →</a>
           </div>
         </section>
